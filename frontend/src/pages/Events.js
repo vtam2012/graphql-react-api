afa3a4a91a2a52f4934a55d0b4ca04361b5b24b7
Vincent Tam <vtam2012@gmail.com>
2019-02-11 19:34:32 -0800

    Added names for queries and fixed varibles in them

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -51,8 +51,8 @@ class EventsPage extends Component {
    
         const requestBody = {
                 query: `
-                    mutation {
-                        createEvent(eventInput: {title: "${title}" , description:"${description}", price: ${price}, date: ${date} }) {
+                    mutation CreateEvent($title: String!, $desc: String!, $price: Float!, $date: String!) {
+                        createEvent(eventInput: {title: $title, description: $desc, price: $price, date: $date }) {
                             _id
                             title
                             description
@@ -60,7 +60,13 @@ class EventsPage extends Component {
                             price
                         }
                     }
-                `
+                `,
+                variables: {
+                    title: title,
+                    desc: description,
+                    price: price,
+                    date: date
+                }
         };
 
         const token = this.context.token;
@@ -163,14 +169,17 @@ class EventsPage extends Component {
         }
         const requestBody = {
             query: `
-            mutation {
-                bookEvent(eventId: "${this.state.selectedEvent._id}") {
-                    _id
-                    createdAt
-                    updatedAt
+                mutation BookEvent($id: ID!) {
+                    bookEvent(eventId: $id) {
+                        _id
+                        createdAt
+                        updatedAt
+                    }
                 }
+            `,
+            variables: {
+                id: this.state.selectedEvent._id
             }
-            `
         };
 
         fetch('http://localhost:8000/graphql', {
